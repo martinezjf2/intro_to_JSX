6079ef0fefcd55398f311d8513d73264d79e745e
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-14 22:09:10 -0500

    using import statements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+// ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
