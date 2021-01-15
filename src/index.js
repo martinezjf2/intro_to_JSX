78f73b2769e60bb05c260ccd222b0e1c65ae30de
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-14 22:10:43 -0500

    create a div render only renders one html element unless a div is invoked

--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a Paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
